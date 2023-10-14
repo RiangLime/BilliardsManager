8033a4c1db1f4b06bf4835f9418b4305dbc809db
Jiaqi <77265945+RiangLime@users.noreply.github.com>
2023-10-15 00:59:03 +0800

    Build Base Project

---
+++ src/main/java/top/lime/billiardsmanager/BilliardsManagerApplication.java
@@ -0,0 +1,13 @@
+package top.lime.billiardsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilliardsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilliardsManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/top/lime/billiardsmanager/BilliardsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package top.lime.billiardsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilliardsManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
